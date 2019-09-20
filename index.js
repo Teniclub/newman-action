d1a72de9b24f909881f60801480248baa28019ed
Tom Hang <tom@spin-interactive.com>
2019-09-20 15:54:05 +0200

    action start

---
+++ index.js
@@ -0,0 +1,13 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const newman = require('@newman');
+
+try {
+    const nameToGreet = core.getInput('who-to-greet');
+    const test = core.getInput('postmanApiKey');
+    console.log("test");
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
